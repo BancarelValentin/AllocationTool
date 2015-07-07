fdcc29e5be9f3c9f5c22a825502b8b396d274e97
Bancarel Valentin @ MBP <bancarel.valentin@gmail.com>
2015-07-07 14:11:53 +0100

    formatting code

--- Allocation-Tools.js
+++ Allocation-Tools.js
@@ -4,174 +4,175 @@ Markers = new Mongo.Collection('markers');
 
 if (Meteor.isClient) {
 
-  Meteor.startup(function() {  
-    GoogleMaps.load();
-  });
+    Meteor.startup(function() {
+        GoogleMaps.load();
+    });
 
-  Template.map.helpers({  
-    mapOptions: function() {
-      if (GoogleMaps.loaded()) {
-        return {
-          center: new google.maps.LatLng(57.140682, -2.106124),
-          zoom: 9,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-      }
-    }
-  });
-
-  Template.map.onCreated(function() {
-    GoogleMaps.ready('map', function(map) {
-
-      var markers = {};
-
-      Trucks.find().observe({
-        added: function(document) {
-          // Create a marker for this document
-          var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(document.location.lat, document.location.lng),
-            map: map.instance,
-            truckID: document.id,
-            id: document._id
-          });
-
-          google.maps.event.addListener(marker, 'click', function() {
-            Meteor.call("showDetails",marker.truckID);
-          });
-
-          // Store this marker instance within the markers object.
-          markers[document._id] = marker;
-        },
-
-        changed: function(newDocument, oldDocument) {
-          markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
-        },
-
-        removed: function(oldDocument) {
-          // Remove the marker from the map
-          markers[oldDocument._id].setMap(null);
-
-          // Clear the event listener
-          google.maps.event.clearInstanceListeners(markers[oldDocument._id]);
-
-          // Remove the reference to this marker instance
-          delete markers[oldDocument._id];
+    Template.map.helpers({
+        mapOptions: function() {
+            if (GoogleMaps.loaded()) {
+                return {
+                    center: new google.maps.LatLng(57.140682, -2.106124),
+                    zoom: 9,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+            }
         }
-      });
-
-    });
-  });
-
-  Template.leftPart.helpers(
-  {
-    trucks: function () {
-      return Trucks.find();
-    }
-  });
-
-  Template.jobItem.helpers({
-    formattedDate:function(){
-      console.log(moment(this.earliestCol.time).format('DD-MM-YYYY'));
-      return moment(this.earliestCol.time).format('DD-MM-YYYY');
-    }
-  })
-
-  Template.rightPart.helpers(
-  {
-    jobs: function(){
-      return Session.get("recommendedJobs");
-    }
-  });
-
-  Template.selectedTruck.helpers(
-  {
-    selectedTruck: function () {
-      return Session.get("selectedTruck");
-    }
-  });
-
-  Template.truckItem.events(
-  {
-    "click .truckItem": function(){
-      Meteor.call("showDetails",this.id);
-    }
-  });
-
-  Meteor.methods({
-    showDetails: function(id){
-      Session.set("selectedTruck",Trucks.findOne({id:id}));
-      Session.set("recommendedJobs", getSortedJobForTruck(id));
-    }
-  })
-  function getSortedJobForTruck (truckId) {
-var truck = Trucks.findOne({id:truckId})
-   var recommendedJobs = Jobs.find().fetch();//get recommended jobs for that truck
-   var outstandingJobs = Jobs.find().fetch();//all outstandings jobs
-
-
-    outstandingJobs.forEach(function (job) {
-      recommendedJobs.forEach(function (jobR) {
-         if(jobR.id == job.id){
-            job.isRecommended = true;
-         }
-      });
-
-      console.log(job);
-     job.truckSelectedName = truck.name;
-      var dist = getStraigthDistanceBetweenLocations({
-          lat: job.latestDel.location.lat,
-          lon: job.latestDel.location.long
-      }, {
-          lat: truck.location.lat,
-          lon: truck.location.lng
-      });
-      var km = dist.toString().split(".")[0];
-      var m = dist.toString().split(".")[1];
-      if (m) {
-          m = "." + m.substring(0, 3);
-      } else {
-          m = ".0";
-      }
-      job.distance = km + m;
     });
 
-   
+    Template.map.onCreated(function() {
+        GoogleMaps.ready('map', function(map) {
 
-    var sortedJobs = outstandingJobs.sort(function(jobx, joby) {
-      return (jobx.recommended && joby.recommended) ? 0 : (jobx.recommended) ? -1 : (joby.recommended) ? 1 : jobx.distance - joby.distance;
+            var markers = {};
+
+            Trucks.find().observe({
+                added: function(document) {
+                    // Create a marker for this document
+                    var marker = new google.maps.Marker({
+                        position: new google.maps.LatLng(document.location.lat, document.location.lng),
+                        map: map.instance,
+                        truckID: document.id,
+                        id: document._id
+                    });
+
+                    google.maps.event.addListener(marker, 'click', function() {
+                        Meteor.call("showDetails", marker.truckID);
+                    });
+
+                    // Store this marker instance within the markers object.
+                    markers[document._id] = marker;
+                },
+
+                changed: function(newDocument, oldDocument) {
+                    markers[newDocument._id].setPosition({
+                        lat: newDocument.lat,
+                        lng: newDocument.lng
+                    });
+                },
+
+                removed: function(oldDocument) {
+                    // Remove the marker from the map
+                    markers[oldDocument._id].setMap(null);
+
+                    // Clear the event listener
+                    google.maps.event.clearInstanceListeners(markers[oldDocument._id]);
+
+                    // Remove the reference to this marker instance
+                    delete markers[oldDocument._id];
+                }
+            });
+
+        });
     });
 
-    console.log(sortedJobs);
-    return sortedJobs;
-  }
+    Template.leftPart.helpers({
+        trucks: function() {
+            return Trucks.find();
+        }
+    });
+
+    Template.jobItem.helpers({
+        formattedDate: function() {
+            console.log(moment(this.earliestCol.time).format('DD-MM-YYYY'));
+            return moment(this.earliestCol.time).format('DD-MM-YYYY');
+        }
+    })
+
+    Template.rightPart.helpers({
+        jobs: function() {
+            return Session.get("recommendedJobs");
+        }
+    });
+
+    Template.selectedTruck.helpers({
+        selectedTruck: function() {
+            return Session.get("selectedTruck");
+        }
+    });
+
+    Template.truckItem.events({
+        "click .truckItem": function() {
+            Meteor.call("showDetails", this.id);
+        }
+    });
+
+    Meteor.methods({
+        showDetails: function(id) {
+            Session.set("selectedTruck", Trucks.findOne({
+                id: id
+            }));
+            Session.set("recommendedJobs", getSortedJobForTruck(id));
+        }
+    })
+
+    function getSortedJobForTruck(truckId) {
+        var truck = Trucks.findOne({
+            id: truckId
+        })
+        var recommendedJobs = Jobs.find().fetch(); //get recommended jobs for that truck
+        var outstandingJobs = Jobs.find().fetch(); //all outstandings jobs
+
+
+        outstandingJobs.forEach(function(job) {
+            recommendedJobs.forEach(function(jobR) {
+                if (jobR.id == job.id) {
+                    job.isRecommended = true;
+                }
+            });
+
+            console.log(job);
+            job.truckSelectedName = truck.name;
+            var dist = getStraigthDistanceBetweenLocations({
+                lat: job.latestDel.location.lat,
+                lon: job.latestDel.location.long
+            }, {
+                lat: truck.location.lat,
+                lon: truck.location.lng
+            });
+            var km = dist.toString().split(".")[0];
+            var m = dist.toString().split(".")[1];
+            if (m) {
+                m = "." + m.substring(0, 3);
+            } else {
+                m = ".0";
+            }
+            job.distance = km + m;
+        });
 
 
 
-  function getStraigthDistanceBetweenLocations(loc1, loc2) {
-    console.log(loc1,loc2);
-      var dist = 0.0;
-      var lon1 = (loc1.lon * Math.PI) / 180;
-      var lon2 = (loc2.lon * Math.PI) / 180;
-      var lat1 = (loc1.lat * Math.PI) / 180;
-      var lat2 = (loc2.lat * Math.PI) / 180;
-      var R = 6373;
-      dist = Math.acos((Math.sin(lat1) * Math.sin(lat2)) + (Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2))) * R;
-      return dist;
-  }
+        var sortedJobs = outstandingJobs.sort(function(jobx, joby) {
+            return (jobx.recommended && joby.recommended) ? 0 : (jobx.recommended) ? -1 : (joby.recommended) ? 1 : jobx.distance - joby.distance;
+        });
+
+        console.log(sortedJobs);
+        return sortedJobs;
+    }
+
+
+
+    function getStraigthDistanceBetweenLocations(loc1, loc2) {
+        console.log(loc1, loc2);
+        var dist = 0.0;
+        var lon1 = (loc1.lon * Math.PI) / 180;
+        var lon2 = (loc2.lon * Math.PI) / 180;
+        var lat1 = (loc1.lat * Math.PI) / 180;
+        var lat2 = (loc2.lat * Math.PI) / 180;
+        var R = 6373;
+        dist = Math.acos((Math.sin(lat1) * Math.sin(lat2)) + (Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2))) * R;
+        return dist;
+    }
 
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        // code to run on server at startup
+    });
 }
 
 
 UI.registerHelper('formatTime', function(context, options) {
-  if(context)
-    return moment(context).fromNow();
+    if (context)
+        return moment(context).fromNow();
 });
-
-
-
